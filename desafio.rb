65dd77b491931f42a41abbf27771b7d04aa403bc
Cabayer915 <cauaribeirob915@gmail.com>
2024-09-19 04:02:07 -0300

    Adicionando desafio em Ruby da DIO

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+puts "Bem vindo ao departamento de MKT!"
+puts "Preencha seu cadastro."
+
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "Cadastro concluido. Seu nome é #{nome} e sua idade é #{idade}."
