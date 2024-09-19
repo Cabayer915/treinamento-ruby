693a594c5a529c5a65d9a1c9b6ec444bb29e4543
Cabayer915 <cauaribeirob915@gmail.com>
2024-09-19 03:22:39 -0300

    Adicionando conceitos de operadores no Ruby

---
+++ operadores.rb
@@ -0,0 +1,22 @@
+# Operadores matematicos em Ruby
+
+num1 = 12
+num2 = 10
+
+# Soma
+puts num1 + num2
+
+# Subtração
+puts num1 - num2
+
+# Multiplicação
+puts num1 * num2
+
+# Divisão (Retorna o Quociente ou número real da divisão)
+puts num1 / num2
+
+# Módulo (Retorna o resto da divisão, realizando a operação até o sobrar o resto)
+puts num1 % num2
+
+# Exponenciação
+puts num1 ** num2
